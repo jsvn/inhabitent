0db6bd9e667b99b8165fc6f8c2977e47ff1cf581
jasveen <jsvn.gill@gmail.com>
2017-02-24 20:05:49 -0800

    update shop page

--- themes/inhabitent/template-parts/content-products.php
+++ themes/inhabitent/template-parts/content-products.php
@@ -11,14 +11,14 @@
     <header class="entry-header">
         <?php if ( has_post_thumbnail() ) : ?>
 
-             <div class="thumbnail-wrapper">
+            <div class="thumbnail-wrapper">
             <?php the_post_thumbnail( 'large' ); ?>
-          </div>
+            </div>
         <!-- <?php endif; ?> -->
-
-        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-        <p><?php echo CFS()->get('price'); ?></p>
-
+        <div class="product-info">
+          <?php the_title( sprintf( '<h2 class="entry-title"><span a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></span></h2>' ); ?>
+          <p><?php echo CFS()->get('price'); ?></p>
+        </div>
         <?php if ( 'post' === get_post_type() ) : ?>
 
         <?php endif; ?>
